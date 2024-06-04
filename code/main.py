519d069a32298c6682e2cfc8d6ba037211240b23
Ecsedi Tamás József <ecsi.hgh@gmail.com>
2024-06-04 21:48:34 +0200

    restart, start/end texts, finomítás, minden kész

--- code/main.py
+++ code/main.py
@@ -15,6 +15,65 @@ class Game:
         self.extra_timer = 20000
         self.update_text = None
         self.update_enemy_text = None
+        self.starting = True
+
+        # rect
+        self.rect_width = WIDTH
+        self.rect_height = 10
+        self.rect_color = (0, 0, 255)
+        self.rect_y = -20
+        self.update_anime = False
+
+        # Player setup
+        player_sprite = Player((WIDTH / 2, screen_height), WIDTH)
+        self.player = pygame.sprite.GroupSingle(player_sprite)
+
+        # health and score setup
+        self.lives = 3
+        self.live_surf = pygame.image.load('../graphics/player.png').convert_alpha()
+        self.live_x_start_pos = WIDTH - (self.live_surf.get_size()[0] * 2 + 20)
+        self.score = 0
+        self.font = pygame.font.Font('../font/Pixeled.ttf', 16)
+
+        # Obstacle setup
+        self.shape = obstacle.shape
+        self.block_size = 6
+        self.blocks = pygame.sprite.Group()
+        self.obstacle_amount = 5
+        self.obstacle_x_positions = [num * (WIDTH / self.obstacle_amount) for num in range(self.obstacle_amount)]
+        self.create_multiple_obstacles(self.obstacle_x_positions, x_start=WIDTH / 15, y_start=HEIGHT - 400)
+
+        # Alien setup
+        self.aliens = pygame.sprite.Group()
+        self.alien_lasers = pygame.sprite.Group()
+        self.alien_setup(rows=7, cols=13)
+        self.alien_direction = 1
+
+        # Extra setup
+        self.extra = pygame.sprite.GroupSingle()
+        self.extra_spawn_time = randint(80, 120)
+
+        # Audio
+        music = pygame.mixer.Sound('../audio/music.wav')
+        music.set_volume(0.5)
+        music.play(loops=-1)
+        self.laser_sound = pygame.mixer.Sound('../audio/laser.wav')
+        self.laser_sound.set_volume(0.5)
+        self.explosion_sound = pygame.mixer.Sound('../audio/explosion.wav')
+        self.explosion_sound.set_volume(0.5)
+
+        # Countdown setup
+        self.countdown_time = 20
+        self.countdown_font = pygame.font.Font('../font/Pixeled.ttf', 40)
+
+    def restart(self):
+        # HEJ upgrades
+        self.alien_laser_speed = 5
+        self.move_down_distance = 2
+        self.alien_timer = 1100
+        self.extra_timer = 20000
+        self.update_text = None
+        self.update_enemy_text = None
 
         # rect
         self.rect_width = WIDTH
@@ -148,13 +207,11 @@ class Game:
                 # obstacle collisions
                 if pygame.sprite.spritecollide(laser, self.blocks, True):
                     laser.kill()
+                    self.score -= 150
 
                 if pygame.sprite.spritecollide(laser, self.player, False):
                     laser.kill()
-                    self.lives -= 1
-                    if self.lives <= 0:
-                        pygame.quit()
-                        sys.exit()
+                    self.lives -= 1                 
 
         # aliens
         if self.aliens:
@@ -195,6 +252,17 @@ class Game:
             victory_surf = self.font.render('JOB DONE!', False, 'white')
             victory_rect = victory_surf.get_rect(center=(WIDTH / 2, screen_height / 2))
             screen.blit(victory_surf, victory_rect)
+            victory2_surf = self.font.render('Press  ENTER  to  restart!', False, 'yellow')
+            victory2_rect = victory2_surf.get_rect(center=(WIDTH / 2, screen_height -100))
+            screen.blit(victory2_surf, victory2_rect)
+
+    def defeat_message(self):
+        defeat_surf = self.font.render('JOB FAILED!', False, 'white')
+        defeat_rect = defeat_surf.get_rect(center=(WIDTH / 2, screen_height / 2))
+        screen.blit(defeat_surf, defeat_rect)
+        victory2_surf = self.font.render('Press  ENTER  to  restart!', False, 'yellow')
+        victory2_rect = victory2_surf.get_rect(center=(WIDTH / 2, screen_height -100))
+        screen.blit(victory2_surf, victory2_rect)
 
     def extra_function(self):
         self.update_anime = True
@@ -225,7 +293,7 @@ class Game:
         elif chosen_player_upgrade == player_c:
             if self.player.sprite.laser_speed > -20:
                 self.player.sprite.laser_speed = chosen_player_upgrade
-                self.update_text = 'Player laser speed increased'
+                self.update_text = 'Player  Laser  Speed  Increased'
             else:
                 self.update_text = 'Nothing  Happened'
 
@@ -233,55 +301,69 @@ class Game:
         if chosen_alien_upgrade == alien_a:
             if self.alien_laser_speed < 15:
                 self.alien_laser_speed = chosen_alien_upgrade
-                self.update_enemy_text = 'Alien  Laser  Speed increased'
+                self.update_enemy_text = 'Alien  Laser  Speed  Increased'
             else:
-                self.update_enemy_text = 'Nothing  Happened with the Enemy'
+                self.update_enemy_text = 'Nothing  Happened  With  The  Enemy'
         elif chosen_alien_upgrade == alien_b:
             if self.alien_timer > 500:
                 self.alien_timer = chosen_alien_upgrade
                 pygame.time.set_timer(ALIENLASER, self.alien_timer)
                 self.update_enemy_text = 'Alien  Laser  Cooldown  Decreased'
             else:
-                self.update_enemy_text = 'Nothing  Happened with the Enemy'
+                self.update_enemy_text = 'Nothing  Happened  With  The  Enemy'
         elif chosen_alien_upgrade == alien_c:
             if self.alien_direction < 10:
                 self.alien_direction = chosen_alien_upgrade
                 self.update_enemy_text = 'Alien  Speed  Increased'
             else:
-                self.update_enemy_text = 'Nothing  Happened with the Enemy'
+                self.update_enemy_text = 'Nothing  Happened  With  The  Enemy'
 
     def animation(self):
         if self.update_anime:
-            self.rect_y += 40
+            self.rect_y += 50
         if self.rect_y >= HEIGHT:
             self.rect_y = -20
             self.update_anime = False
 
     def run(self):
-        self.player.update()
-        self.alien_lasers.update()
-        self.extra.update()
+        if self.lives > 0:
+            self.player.update()
+            self.alien_lasers.update()
+            self.extra.update()
+            self.aliens.update(self.alien_direction)
+            self.alien_position_checker()
+            self.extra_alien_timer()
+            self.collision_checks()
+            self.player.sprite.lasers.draw(screen)
+            self.player.draw(screen)
+            self.blocks.draw(screen)
+            self.aliens.draw(screen)
+            self.alien_lasers.draw(screen)
+            self.extra.draw(screen)
+            self.display_lives()
+            self.display_score()
+            self.display_countdown()
+            self.victory_message()
 
-        self.aliens.update(self.alien_direction)
-        self.alien_position_checker()
-        self.extra_alien_timer()
-        self.collision_checks()
+            # Rect animation
+            self.animation()
+            pygame.draw.rect(screen, self.rect_color, (0, self.rect_y, self.rect_width, self.rect_height))
 
-        self.player.sprite.lasers.draw(screen)
-        self.player.draw(screen)
-        self.blocks.draw(screen)
-        self.aliens.draw(screen)
-        self.alien_lasers.draw(screen)
-        self.extra.draw(screen)
-        self.display_lives()
-        self.display_score()
-        self.display_countdown()
-        self.victory_message()
+        if self.lives <= 0:
+            self.player.empty()
+            self.alien_lasers.empty()
+            self.aliens.empty()
+            self.defeat_message()
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_RETURN]:
+                self.restart()
 
-        # Rect animation
-        self.animation()
-        pygame.draw.rect(screen, self.rect_color, (0, self.rect_y, self.rect_width, self.rect_height))
-
+        if not self.aliens.sprites():
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_RETURN]:
+                self.alien_lasers.empty()
+                self.player.empty()
+                self.restart()
 
 class CRT:
     def __init__(self):
@@ -300,6 +382,28 @@ class CRT:
         self.create_crt_lines()
         screen.blit(self.tv, (0, 0))
 
+def starting_message():
+        global starting
+        font = pygame.font.Font('../font/Pixeled.ttf', 22)
+        starting_surf = font.render('YOUR  JOB:', False, 'white')
+        starting_rect = starting_surf.get_rect(center=(WIDTH / 2, screen_height / 2))
+        screen.blit(starting_surf, starting_rect)
+
+        starting2_surf = font.render('DESTROY  THE  SPACE  INVADERS ', False, 'red')
+        starting2_rect = starting2_surf.get_rect(center=(WIDTH / 2, screen_height / 2+60))
+        screen.blit(starting2_surf, starting2_rect)
+
+        starting3_surf = font.render('TO  SAVE  MOTHER  EARTH !', False, 'white')
+        starting3_rect = starting3_surf.get_rect(center=(WIDTH / 2, screen_height / 2+120))
+        screen.blit(starting3_surf, starting3_rect)
+
+        victory2_surf = font.render('Press  ENTER  to  Start!', False, 'yellow')
+        victory2_rect = victory2_surf.get_rect(center=(WIDTH / 2, screen_height -100))
+        screen.blit(victory2_surf, victory2_rect)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RETURN]:
+            starting = False
 
 if __name__ == '__main__':
     pygame.init()
@@ -309,36 +413,47 @@ if __name__ == '__main__':
     screen_height = HEIGHT
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
-    game = Game()
     crt = CRT()
-
-    #időzítők
-    alientimer = game.alien_timer
-    ALIENLASER = pygame.USEREVENT + 1
-    pygame.time.set_timer(ALIENLASER, alientimer)
-
-    extratimer = game.extra_timer
-    EXTRATIMER = pygame.USEREVENT + 2
-    pygame.time.set_timer(EXTRATIMER, extratimer)
-
-    COUNTDOWN = pygame.USEREVENT + 3
-    pygame.time.set_timer(COUNTDOWN, 1000)
+    starting = True
+    game = None
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type == ALIENLASER:
+            if not starting and event.type == ALIENLASER:
                 game.alien_shoot()
-            if event.type == EXTRATIMER:  # Correct handling for the extra timer
+            if not starting and event.type == EXTRATIMER:  # Correct handling for the extra timer
                 game.countdown_time = 20
                 game.extra_function()
-            if event.type == COUNTDOWN:
+            if not starting and event.type == COUNTDOWN:
                 game.countdown_time -= 1
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            pygame.quit()
+            sys.exit()
+
 
         screen.fill((30, 30, 30))
-        game.run()
+        if starting:
+            starting_message()
+        if not starting:
+            if game is None:
+                game = Game()
+                # időzítők
+                alientimer = game.alien_timer
+                ALIENLASER = pygame.USEREVENT + 1
+                pygame.time.set_timer(ALIENLASER, alientimer)
+
+                extratimer = game.extra_timer
+                EXTRATIMER = pygame.USEREVENT + 2
+                pygame.time.set_timer(EXTRATIMER, extratimer)
+
+                COUNTDOWN = pygame.USEREVENT + 3
+                pygame.time.set_timer(COUNTDOWN, 1000)
+
+            game.run()
         crt.draw()
 
         pygame.display.flip()
